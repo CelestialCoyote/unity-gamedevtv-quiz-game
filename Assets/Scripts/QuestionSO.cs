04234f46e16ea346fd8cfcc04ac52cbd61dee266
Paul Stearns <celestialcoyote@gmail.com>
2023-06-23 20:00:01 -0700

    scriptable object

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+
 public class QuestionSO : ScriptableObject
 {
 	[TextArea(2, 6)]
     [SerializeField] string queston = "Enter new question text here.";
+	[SerializeField] string[] answers = new string[4];
+	[SerializeField] int correctAnswerIndex;
 
 	public string GetQuestion()
 	{
 		return question;
+	}
+
+	public string GetAnswer(int index)
+	{
+		return answes[index];
+	}
+
+	public int GetCorrectAnswerIndex()
+	{
+		return correctAnswerIndex;
 	}
 }
